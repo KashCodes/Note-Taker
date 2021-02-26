ea1d360cb93cf3eb9bf172dfea74b2478d4e9c59
Kashay Arbelo <kashay@agyeman.me>
2021-02-25 20:00:18 -0700

    properly connected express server. Fixed caching issues.

--- server.js
+++ server.js
@@ -5,23 +5,25 @@ const path = require('path');
 
 // instantiate the server, to tell it to listen for requests
 const app = express();
+// tell our app (heroku) to use that port, if it has been set, and if not, default to port 80.
+const PORT = process.env.PORT || 3005;
+
+const { db } = require('./db/db.json');
+
+//-----------------Middleware Start-----------------//
+const nocache = require("nocache");
+
+app.use(nocache());
+app.set('etag', false);
+//-----------------Middleware End-----------------//
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// route that the front-end can request data from
+app.get('/db/db', (req, res) => {
+  res.send('Hello!');
+});
 
 // listen for server
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(3005, () => {
+  console.log(`API server now on port 3005!`);
 });
