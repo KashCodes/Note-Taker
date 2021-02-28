81301b59653f32b65efdecf2aea6a12109282f85
Kashay Arbelo <kashay@agyeman.me>
2021-02-28 11:45:03 -0700

    added app.get function to look for specific note id's saved and return values

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,6 +5,28 @@ const { v4: uuidv4 } = require('uuid')
 
 module.exports = function(app) {
 
+  // connected the JSON API
+  app.get("/api/notes", (req, res) => {
+    console.log(notes)
+      res.json(notes)
+  })
 
+  // Looking for specific note ID to re-display after it's been saved. 
+  app.get("/api/notes/:id", (req, res) => {
+
+    const id = req.params.id
+    let found
+    // forEach ensures it applies to every note saved. ****
+    notes.forEach(note => {
+      // will only pull notes that have id's
+      if (id == note.id){
+        found = note
+         res.json(note)
+      }
+    })
+     res.json(false)
+  })
+
+  
 
 }
