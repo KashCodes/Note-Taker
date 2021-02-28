8bbf6c5fac7052fabc593c61bde31ffc8d602e89
Kashay Arbelo <kashay@agyeman.me>
2021-02-28 11:30:29 -0700

    installed uuid and set to api routes.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+// Required module connections 
+const fs = require("fs")
+let notes = require("../db/db.json")
+const { v4: uuidv4 } = require('uuid')
+
+module.exports = function(app) {
+
+
+
+}
--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 // tell our app (heroku) to use that port, if it has been set, and if not, default to port 80.
 const PORT = process.env.PORT || 3005;
 
-
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.static('public'));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
